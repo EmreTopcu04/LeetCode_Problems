afa1e68773b79a231fd3d6e99a7982c4b706396c
Emre Topcu <114030199+EmreTopcu04@users.noreply.github.com>
2024-08-29 13:33:02 +0300

    Solution947 Python Added

---
+++ 947. Most Stones Removed with Same Row or Column/Solution947.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def removeStones(self, stones):
+
+        graph = {}
+
+        for x, y in stones:
+            
+            if x not in graph:
+                graph[x] = []
+            if ~y not in graph:
+                graph[~y] = []
+
+            graph[x].append(~y)
+            graph[~y].append(x)
+
+        visited = set()
+
+        def dfs(node):
+            stack = [node]
+            while stack:
+                current = stack.pop()
+                if current not in visited:
+                    visited.add(current)
+                    
+                    for neighbor in graph[current]:
+                        if neighbor not in visited:
+                            stack.append(neighbor)
+            
+
+        components = 0
+
+        for x,y in stones:
+
+            if x not in visited:
+                dfs(x)
+                components += 1
+
+        return len(stones) - components
+       
+                
+
+
+c = Solution()
+
+print(c.removeStones([[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]))
